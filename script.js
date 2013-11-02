f07d0510a8c3dc6efab0037da7f2d81eaca83881
GuillemV98 <98Guill398@gmail.com>
2013-11-02 16:48:14 +0100

    aquí hos traigo una página en la cual podreis escribir comentarios y eliminarlos

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
  * Created by root on 11/2/13.
  */
 
-    $(document).ready(function(){
-        $("#enviar").on('click', function(){
-            var fecha= new Date();
-            var fechaComentario= fecha.getHours()+":"+fecha.getMinutes()+":"+fecha.getSeconds();
+$(document).ready(function() {
+    $("#enviar").on('click',function(){
+        var texto=$("#comentarios").val();
+        var fecha= new Date();
+        var cruz= "<img class='eliminar' src='cancel.png'>"+"</img>";
+        var fechaComentario= "<span class='fecha'>"+fecha.getHours()+":"+fecha.getMinutes()+":"+fecha.getSeconds()+"</span>";
+        var divComentarioEscrito= "<div class='comentario-enviado'>";
+        var divCompleto= divComentarioEscrito+fechaComentario+"   "+texto+"   "+cruz+"</div>";
+        $("#comentarios-escritos").prepend(divCompleto);
+    });
 
-            var texto=$("#comentarios").val();
-            var divComentarioEscrito= "<div class='comentario-enviado'>";
-            var divCompleto=divComentarioEscrito+fechaComentario+texto+"</div>";
-            $("#comentarios-escritos").prepend(divCompleto);
+    // Note the amended selector here...
+    $("#comentarios-escritos").on('click', '.eliminar', function(){
+        alert("ELIMINAR");
     });
 
 });
